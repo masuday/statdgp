868add91aee813381bfc36126ef3dd99fe0b752e
masuday <masdee@gmail.com>
2023-12-22 15:04:42 +0900

    add data (ethanol)

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' @title
+#'    Measurements of boiling point of ethanol
+#' @description
+#'    It is an artificial data set to demonstrate error in observations.
+#' @format
+#'    A vector with 9 elements.
+"ethanol"
---
+++ data-raw/make_data.R
@@ -0,0 +1,3 @@
+# ethabol boiling point
+ethanol <- c(77.9, 78.3, 77.4, 78.5, 78.8, 79.0, 78.0, 79.3, 77.6)
+save(ethanol, file="ethanol.rda")
